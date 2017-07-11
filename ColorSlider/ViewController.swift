4a96c3264fff8dca458872c16c21e5b3f30a58ea
Sujanth <sujanth1987@gmail.com>
2017-07-11 20:24:46 +1000

    main functions completed

--- ColorSlider/ViewController.swift
+++ ColorSlider/ViewController.swift
@@ -9,17 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var greyLabel: UILabel!
+    @IBOutlet weak var greySlider: UISlider!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        greyAction(greenSlider)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func greyAction(_ sender: UISlider) {
+        view.backgroundColor = UIColor(white: CGFloat(sender.value), alpha: 1)
+        
+        greyLabel.text = String(format: "%.2f", sender.value)
+        
+        if sender.value >= 0.5 {
+            greyLabel.textColor = UIColor(white: 0, alpha: 1)
+            redLabel.textColor = UIColor(white: 0, alpha: 1)
+            greenLabel.textColor = UIColor(white: 0, alpha: 1)
+            blueLabel.textColor = UIColor(white: 0, alpha: 1)
+        } else {
+            greyLabel.textColor = UIColor(white: 1, alpha: 1)
+            redLabel.textColor = UIColor(white: 1, alpha: 1)
+            greenLabel.textColor = UIColor(white: 1, alpha: 1)
+            blueLabel.textColor = UIColor(white: 1, alpha: 1)
+        }
     }
 
-
+    @IBAction func rgbAction(_ sender: UISlider) {
+        
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        
+        if redSlider.value >= 0.5 && greenSlider.value >= 0.5 && blueSlider.value >= 0.5 {
+            greyLabel.textColor = UIColor(white: 0, alpha: 1)
+            redLabel.textColor = UIColor(white: 0, alpha: 1)
+            greenLabel.textColor = UIColor(white: 0, alpha: 1)
+            blueLabel.textColor = UIColor(white: 0, alpha: 1)
+        } else {
+            greyLabel.textColor = UIColor(white: 1, alpha: 1)
+            redLabel.textColor = UIColor(white: 1, alpha: 1)
+            greenLabel.textColor = UIColor(white: 1, alpha: 1)
+            blueLabel.textColor = UIColor(white: 1, alpha: 1)
+        }
+    }
+    
+    @IBAction func reset() {
+    }
 }
 
